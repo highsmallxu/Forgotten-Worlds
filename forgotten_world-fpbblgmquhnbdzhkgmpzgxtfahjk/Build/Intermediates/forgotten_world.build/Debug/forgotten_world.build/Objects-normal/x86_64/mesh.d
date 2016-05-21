622c427cea33f0929df07a95ce8fe53a2eb3c1b7
Xiaoxu <highsmallxu@gmail.com>
2016-05-21 22:37:27 +0200

    Add files via upload
    implement exercise2

---
+++ forgotten_world-fpbblgmquhnbdzhkgmpzgxtfahjk/Build/Intermediates/forgotten_world.build/Debug/forgotten_world.build/Objects-normal/x86_64/mesh.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/gaoxiaoxu/Dropbox/TU_Delft/Quarter3/3D\ Computer\ Graphic/Exercise3/AppearanceControl/mesh.cpp \
+  /Users/gaoxiaoxu/Dropbox/TU_Delft/Quarter3/3D\ Computer\ Graphic/Exercise3/AppearanceControl/mesh.h \
+  /Users/gaoxiaoxu/Dropbox/TU_Delft/Quarter3/3D\ Computer\ Graphic/Exercise3/AppearanceControl/Vertex.h \
+  /Users/gaoxiaoxu/Dropbox/TU_Delft/Quarter3/3D\ Computer\ Graphic/Exercise3/AppearanceControl/Vec3D.h
