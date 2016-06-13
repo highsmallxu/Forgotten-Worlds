602a1db8ceb759c650b1cba43f68e6fd43a72080
GAOXiaoXu <highsmallxu@gmail.com>
2016-06-13 09:35:24 +0200

    birds added

--- Code/main.cpp
+++ Code/main.cpp
@@ -9,38 +9,74 @@
 #include <assert.h>
 #include "mesh.h"
 #include "imageloader.h"
-#include "glm/glm.hpp"
-
-
+#include "traqueboule.h"
 
 using namespace std;			// make std accessible
+#define PI 3.14159265
 
+//****define variables****//
 
-//0-load bmp [background]
+///////background//////////
+
 GLuint background_texture;
 float backgroundX;
 
+///////objects////////////
+
+Mesh hero;
+Mesh bird;
+Mesh boss;
+
+///////lighting////////
+
+std::vector<Vec3Df> lighting;
+std::vector<Vec3Df> LightPos;
+std::vector<Vec3Df> LightColor;
+Vec3Df CamPos = Vec3Df(0.0f,0.0f,4.0f);
+
+///////texture///////
+
+GLuint _textureId1;
+GLuint _textureId2;
+GLuint _textureId3;
+GLuint texture;
+GLUquadric *quad;
+
+///////move control///////
+
+float x=0;
+float bird_up=0;
+float bird_on=3;
+float boss_on=3;
+float r;
+float count_number=0;
+bool up=true;
+bool down=false;
+bool bird1 = true;
+bool bird2 = true;
+bool bird3 = true;
+bool boss_stop = false;
+double direction = 90;
+double updown = 0;
+double leftright = 0;
+
+
+//****load bmp [background]****
 GLuint bmp_texture_load(const char *filename, int width, int height)
 {
     GLuint texture;
-    
     unsigned char * data;
-    
     FILE * file;
-    
     file = fopen( filename, "rb" );
-    
     if ( file == NULL )
     {
         std::cout<<"File not found";
         return 0;
     }
-    
     data = (unsigned char *)malloc( width * height * 3 );
     //int size = fseek(file,);
     fread( data, width * height * 3, 1, file );
     fclose( file );
-    
     for(int i = 0; i < width * height ; ++i)
     {
         int index = i*3;
@@ -50,24 +86,17 @@ GLuint bmp_texture_load(const char *filename, int width, int height)
         
         data[index] = R;
         data[index+2] = B;
-        
     }
-    
-    
     glGenTextures( 1, &texture );
     glBindTexture( GL_TEXTURE_2D, texture );
     glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE,GL_MODULATE );
     glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_NEAREST );
-    
-    
     glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR );
     glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_REPEAT );
     glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_REPEAT );
     gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width, height,GL_RGB, GL_UNSIGNED_BYTE, data );
     free( data );
-    
     std::cout<<"Texture ID: "<<texture;
-    
     return texture;
 }
 
@@ -76,17 +105,53 @@ void init_background()
     background_texture = bmp_texture_load("background.bmp", 2040, 768);
     backgroundX = 0.0;
 }
+//0.3-compute lighting
+
+
+Vec3Df computeLighting(Vec3Df & vertexPos, Vec3Df & normal, unsigned int light, unsigned int index)
+{
+    
+    Vec3Df lightv = LightPos[0] - vertexPos;
+    lightv.normalize();
+    
+    Vec3Df V = getCameraPosition() - vertexPos;
+    V.normalize();
+    Vec3Df H = (V + lightv) / 2;
+    H.normalize();
+    float dLighting = Vec3Df::dotProduct(normal, lightv);
+    float sLighting = Vec3Df::dotProduct(H, normal);
+    sLighting = pow(sLighting, 3);
+    
+    return Vec3Df(dLighting+sLighting,dLighting+sLighting,dLighting+sLighting);
+}
+void computeLighting()
+{
+    std::vector<Vec3Df> *result=&lighting;
+    for (unsigned int i=0; i<boss.vertices.size();++i)
+    {
+        (*result)[i]=Vec3Df();
+        for (int l=0; l<LightPos.size();++l)
+            (*result)[i]+=computeLighting(boss.vertices[i].p, boss.vertices[i].n, l, i);
+    }
+}
 
 
 //1-load hero,gun
-Mesh hero;
-Mesh gun;
+
+
 
 void init_hero(const char * fileName){
     hero.loadMesh(fileName);
 }
-void init_gun(const char * fileName){
-    gun.loadMesh(fileName);
+void init_bird(const char * fileName){
+    bird.loadMesh(fileName);
+}
+void init_boss(const char * fileName){
+    boss.loadMesh(fileName);
+    lighting.resize(boss.vertices.size());
+    LightPos.push_back(Vec3Df(1,2,7));
+    LightColor.push_back(Vec3Df(0,1,0));
+    computeLighting();
 }
 
 //2-load bullet texture mapping
@@ -109,10 +174,7 @@ GLuint loadTexture(Image* image) {
     return textureId; //Returns the id of the texture
 }
 //2.2-load Image
-GLuint _textureId1;
-GLuint _textureId2;
-GLuint _textureId3;
-GLUquadric *quad;
+
 void init_bullet_texture(){
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_NORMALIZE);
@@ -130,24 +192,29 @@ void init_bullet_texture(){
     delete image3;
 }
 
+
 //3-bullet control
-float x=1.8;
-float r;
-GLuint texture;
+
+
 void update(int value)
 {
-    r+=2.0f; //make bullet keep rotating
-    backgroundX += 0.0015; //background keep moving
+    
+    //bullet keep rotating
+    r+=2.0f;
     if(r>360.f)
     {
         r-=360;
     }
-    x+=0.1f;  //make bullet keep moving forward
-    if(x>5.f)
+    
+    //bullet keep moving forward
+    x+=0.05f;
+    if(x>4.f)
     {
-        x=1.8f;
+        x=0.f;
     }
-    if(texture != _textureId1) //change texture of bullet from frame to frame
+    
+    //bullet change texture
+    if(texture != _textureId1)
     {
         texture = _textureId1;
     }
@@ -156,7 +223,62 @@ void update(int value)
         texture = _textureId2;
     }
     
+    //backgfround keep moving
+    backgroundX += 0.0015;
     
+    
+    //bird keep moving up and down
+    if(up && !down)
+    {
+        bird_up+=0.02;
+        if(bird_up>1){
+            up=false;
+            down=true;
+        }
+    }
+    else if (!up && down)
+    {
+        bird_up-=0.02;
+        if(bird_up<-1){
+            up=true;
+            down=false;
+        }
+    }
+    
+    //bird show up and moving forward
+    if(count_number>10){
+        bird_on-=0.02;
+        if(bird_on<0){
+            bird_on=0;
+        }
+    }
+    
+    //bird disapper
+    double bullet_angle = tan ( direction * PI / 180.0 );
+    if(bird_up/1.6-bullet_angle<0.1 && bird_up/1.6-bullet_angle>-0.1){
+        bird1=false;
+    }
+    else if(bird_up/2.6-bullet_angle<0.1 && bird_up/2.6-bullet_angle>-0.1){
+        bird2=false;
+    }
+    else if(bird_up/3.6-bullet_angle<0.1 && bird_up/3.6-bullet_angle>-0.1){
+        bird3=false;
+    }
+    
+    //boss show up
+    if(!bird1 && !bird2 && !bird3){
+        boss_on-=0.02;
+        if(!boss_stop){
+            if(boss_on<0){
+                boss_on=2;
+                boss_stop=true;
+            }
+        }
+    }
+    
+    
+
+    count_number+=1;
     glutPostRedisplay();
     glutTimerFunc(25,update,0);
 }
@@ -164,66 +286,100 @@ void update(int value)
 
 //4-display function
 //4.1-load bullet
-double direction = 0;
-double updown = 0;
-double leftright = 0;
+
 
 void load_bullet(){
+    
+    //round bullet
     glPushMatrix();
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, texture);
+    
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glRotated(direction,0,0,-10);
-    glTranslatef(x,updown,-10);
-    glRotatef(r,0.0f,0.0f,1.0f);
-    glScaled(0.1f, 0.1f, 0.1f);
-    gluQuadricTexture(quad,1);
-    gluSphere(quad,2,20,20);
-    
+        glTranslated(-1.6, -0.2, -1);
+        glRotated(direction,0,0,1);
+        glTranslatef(x,0,0);
+        glScaled(0.05f, 0.05f, 0.05f);
+        gluQuadricTexture(quad,2);
+        gluSphere(quad,2,20,20);
     glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
     
+    
+    //stick weapon
+    glPushMatrix();
+         glEnable(GL_TEXTURE_2D);
+         glBindTexture(GL_TEXTURE_2D, texture);
+         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+         glTranslated(-1.6, -0.2, -1);
+         glRotated(direction,0,0,1);
+         glTranslatef(0,0,0);
+    //     glRotatef(r,0.0f,0.0f,1.0f);
+         glScaled(0.3f, 0.03f, 0.05f);
+         gluQuadricTexture(quad,2);
+         gluSphere(quad,2,20,20);
+         
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
 }
 
 //4.2-load hero
 void load_hero(){
     glPushMatrix();
-    
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, _textureId3);
-    
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-//    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-//    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); //
-
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-    glDisable(GL_TEXTURE_2D);
-
-
-    glTranslatef(leftright, updown, -5);
-
-    glRotated(45, 0, 1, 0);
-    hero.draw();
-    
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, _textureId3);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glRotated(-20, 0, 1, 0);
+        glTranslated(-2, 0, 0);
+        hero.drawSmooth();
     glPopMatrix();
 }
 
-//4.3-load gun
-void load_gun(){
-    glPushMatrix();
-    glRotated(direction,0,0,-5);
-    glTranslatef(leftright, updown,-5);
-    glScaled(0.5f, 0.5f, 0.5f);
-    gun.draw();
-    
-    glPopMatrix();
-    
+//4.3-load bird
+void load_bird(){
+    if(bird1){
+        glPushMatrix();
+            glTranslated(bird_on,bird_up,0);
+            glRotatef(270,0.0f,1.0f,0.0f);
+            glScaled(0.5, 0.5, 0.5);
+            bird.drawSmooth();
+        glPopMatrix();
+    }
+    if(bird2){
+        glPushMatrix();
+            glTranslated(bird_on+1,bird_up-0.5,0);
+            glRotatef(270,0.0f,1.0f,0.0f);
+            glScaled(0.5, 0.5, 0.5);
+            bird.drawSmooth();
+        glPopMatrix();
+    }
+    if(bird3){
+        glPushMatrix();
+            glTranslated(bird_on+1.5,bird_up+0.5,0);
+            glRotatef(270,0.0f,1.0f,0.0f);
+            glScaled(0.5, 0.5, 0.5);
+            bird.drawSmooth();
+        glPopMatrix();
+    }
 }
 
+//4.4-load boss
+void load_boss(){
+    if(!bird1 && !bird2 && !bird3){
+        glPushMatrix();
+            glTranslated(boss_on, 0, 0);
+            boss.drawWithColors(lighting,1);
+        glPopMatrix();
+    }
+}
+
+
+//4.5-load background
 void load_background()
 {
     glMatrixMode(GL_PROJECTION);
@@ -232,15 +388,11 @@ void load_background()
     gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
-    
-    
-    glLoadIdentity();
-    glDisable(GL_LIGHTING);
-    
-    
-    glColor3f(1,1,1);
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, background_texture);
+        glLoadIdentity();
+        glDisable(GL_LIGHTING);
+        glColor3f(1,1,1);
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, background_texture);
     
     
     // Draw a textured quad
@@ -254,36 +406,50 @@ void load_background()
     
     glDisable(GL_TEXTURE_2D);
     glPopMatrix();
-    
-    
     glMatrixMode(GL_PROJECTION);
     glPopMatrix();
-    
     glMatrixMode(GL_MODELVIEW);
-
+    
 }
 
-
+float lpos[] = { 1, 1, 1, 0 };
 void drawScene() {
     
     glDisable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT |		// clear the frame buffer (color)
+            GL_DEPTH_BUFFER_BIT);		// clear the depth buffer (depths)
     
-    //End of loading background
+
     load_background();
     
-    glLoadIdentity();
-    
-    //load bullet
-
-    load_bullet();
-
-    glDisable(GL_TEXTURE_2D);
-    
-    //load hero
-    load_hero();
-    load_gun();
-    
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);		// enable lighting
+    glEnable(GL_LIGHT0);		// enable
+    glLightfv(GL_LIGHT0, GL_POSITION, lpos);
+
+    load_hero();
+    load_bullet();
+    
+    if(count_number>10){
+        load_bird();
+    }
+
+    load_boss();
+    
+    
+
+
+//
+//    glLoadIdentity();
+//    
+//    //load bullet
+//    
+//    load_bullet();
+//    
+//    glDisable(GL_TEXTURE_2D);
+    
+
+    
     glFlush();
     
     glutSwapBuffers();
@@ -301,7 +467,7 @@ void keyboard(unsigned char k, int x, int y)
             direction -= 5;
             break;
         case 'g':
-             updown +=0.3;
+            updown +=0.3;
             break;
         case 'h':
             updown -=0.3;
@@ -326,9 +492,35 @@ void handleResize(int w, int h) {
     gluPerspective(40, (float)w / (float)h, 0.5, 20.0);
 }
 
+//7-intialization
+void init()
+{
+    glClearColor(0, 0, 0, 0);		// background color
+    glClearDepth(1.0);			// background depth value
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, 1, 1, 100);	// setup a perspective projection
+    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    gluLookAt(				// set up the camera
+              0.0, 0.0, 4.0,		// eye position
+              0.0, 0.0, 0.0,		// lookat position
+              0.0, 1.0, 0.0);		// up direction
+    
+    glEnable(GL_DEPTH_TEST);		// enable hidden surface removal
+    
+//    glEnable(GL_LIGHTING);		// enable lighting
+//    glEnable(GL_LIGHT0);		// enable
+//    glLightfv(GL_LIGHT0, GL_POSITION, lpos);
+
+}
+
 int main(int argc, char * argv[])
 {
-
+    
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(1200,800);
@@ -340,7 +532,8 @@ int main(int argc, char * argv[])
     
     //1-load hero,gun
     init_hero(argc == 2 ? argv[1] : "hero.obj");
-    init_gun(argc == 2 ? argv[1] : "gun.obj");
+    init_bird(argc == 2 ? argv[1] : "bird.obj");
+    init_boss(argc == 2 ? argv[1] : "boss.obj");
     
     //2-load bullet texture mapping
     init_bullet_texture();
@@ -356,8 +549,12 @@ int main(int argc, char * argv[])
     
     //6-reshape function
     glutReshapeFunc(handleResize);
-
-//    glutIdleFunc(bul_move);
+    glutIdleFunc(drawScene);
+    
+    //7-intialization
+    init();
+    
+    //    glutIdleFunc(bul_move);
     
     glutMainLoop();
     return 0; 
